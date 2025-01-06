be239391ae11bbf4fe690c18a6934bde023152ad
Juan Carlos <juansuarezmarin@gmail.com>
2025-01-05 21:20:51 -0500

    #31 patron adataptador - https://www.udemy.com/course/nodejs-de-cero-a-experto/learn/lecture/39408732#overview

--- src/js-foundation/05-factory.js
+++ src/js-foundation/05-factory.js
@@ -1,6 +1,6 @@
 
-const { v4: uuidv4 } = require('uuid');
-const getAge = require('get-age')
+const { getUUID } = require('../js-foundation/get-id.plugin');
+const { getAge } = require('../js-foundation/get-age.plugin');
 
 
 const obj = { name: 'John', birthdate: '1989-11-05' };
@@ -8,7 +8,7 @@ const obj = { name: 'John', birthdate: '1989-11-05' };
 const buildPerson = ({name, birthdate}) => {
 
     return {
-        id: uuidv4(),
+        id: getUUID(),
         name:name,
         birthdate:birthdate,
         age: getAge(birthdate), 
